d8235175a881ac99814c858a5881519fe7192169
PICPPTA <163640985+PICPPTA@users.noreply.github.com>
2025-12-19 16:45:01 +0700

    User.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,14 @@
+// backend/models/User.js
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+export default User;
